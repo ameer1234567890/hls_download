a8f50ec369a49e8b8e593337e35495e702afce76
Ameer Dawood <ameer1234567890@gmail.com>
2018-08-22 21:23:22 +0500

    Modernize

--- hls_download.sh
+++ hls_download.sh
@@ -1,3 +1,4 @@
+#!/bin/sh
 url="$1"
 quality="$2"
 
@@ -5,32 +6,32 @@ download_part(){
   echo "Downloading $partn.... ($i/$num_parts)"
   j=0
   while [ "$native_file_size" != "$remote_file_size" ]; do
-    j="`expr $j + 1`"
+    j="$((j + 1))"
     if [ "$j" -gt 10 ]; then
       echo "Too many re-tries! Exiting...."
       exit 1
     fi
-    curl -o $tmpdir/$partn --progress-bar $hlsq_path$part
-    native_file_size="`stat --printf=\"%s\" $tmpdir/$partn 2> /dev/null`"
-    if [ "$remote_file_size" == "" ]; then
-      remote_file_size="`echo \"$response\" | grep -i 'Content-Length' | awk '{print $2}'`"
+    curl -o "$tmpdir"/"$partn" --progress-bar "$hlsq_path""$part"
+    native_file_size="$(stat --printf="%s" "$tmpdir"/"$partn" 2> /dev/null)"
+    if [ "$remote_file_size" = "" ]; then
+      remote_file_size="$(echo "$response" | grep -i 'Content-Length' | awk '{print $2}')"
     fi
   done
 }
 
-if [ \( "$url" == "-h" \) -o \( "$url" == "--help" \) ]; then
+if [ "$url" = "-h" ] || [ "$url" = "--help" ]; then
   echo "Usage: $0 URL [high|low]"
   exit 0
 fi
 
-if [ "$url" == "" ]; then
+if [ "$url" = "" ]; then
   echo "No URL provided! Exiting...."
   exit 1
 fi
 
-curl -I $url 2>/dev/null > url_check.txt
+curl -I "$url" 2>/dev/null > url_check.txt
 curl_status="$?"
-http_status="`cat url_check.txt | head -n 1 | awk '{print $2}'`"
+http_status="$(< url_check.txt head -n 1 | awk '{print $2}')"
 if [ "$curl_status" != 0 ]; then
   echo "cURL Error: $curl_status. Exiting...."
   rm url_check.txt
@@ -43,110 +44,107 @@ if [ "$http_status" != 200 ]; then
 fi
 rm url_check.txt
 
-video_id="`echo $url | cut -d '/' -f 4`"
+video_id="$(echo "$url" | cut -d '/' -f 4)"
 tmpdir="tmp_$video_id"
-mkdir -p $tmpdir
+mkdir -p "$tmpdir"
 
 tokens=""
 i=0
-while [ "$tokens" == "" ]; do
-  i="`expr $i + 1`"
+while [ "$tokens" = "" ]; do
+  i="$((i + 1))"
   if [ "$i" -gt 10 ]; then
     echo "Too many re-tries! Exiting...."
     exit 1
   fi
-  echo -n "Requesting video page.... "
-  curl -o $tmpdir/page.html --silent $url
-  if [ "`cat $tmpdir/page.html 2> /dev/null`" == "" ]; then
+  printf "Requesting video page.... "
+  curl -o "$tmpdir"/page.html --silent "$url"
+  if [ "$(cat "$tmpdir"/page.html 2> /dev/null)" = "" ]; then
     echo "Invalid response! Exiting...."
     exit 1
   fi
-  has_tokens="`cat $tmpdir/page.html | grep setVideoHLS | cut -d "'" -f 2 | grep '?'`"
-  if [ "$has_tokens" == "" ]; then
+  has_tokens="$(< "$tmpdir"/page.html grep setVideoHLS | cut -d "'" -f 2 | grep '?')"
+  if [ "$has_tokens" = "" ]; then
     tokens=""
     echo "Failed!"
   else
-    tokens="`cat $tmpdir/page.html | grep setVideoHLS | cut -d "'" -f 2 | cut -d '?' -f 2`"
+    tokens="$(< "$tmpdir"/page.html grep setVideoHLS | cut -d "'" -f 2 | cut -d '?' -f 2)"
     echo "Done!"
   fi
   sleep 2
 done
 
-hls_url="`cat $tmpdir/page.html | grep setVideoHLS | cut -d "'" -f 2`"
-if [ "$hls_url" == "" ]; then
+hls_url="$(< "$tmpdir"/page.html grep setVideoHLS | cut -d "'" -f 2)"
+if [ "$hls_url" = "" ]; then
   echo "No HLS sources found! Exiting...."
   exit 1
 fi
 
-echo -n "Requesting HLS lists.... "
-curl -o $tmpdir/hls.m3u8 --silent $hls_url
+printf "Requesting available formats.... "
+curl -o "$tmpdir"/hls.m3u8 --silent "$hls_url"
 echo "Done!"
-if [ "`cat $tmpdir/hls.m3u8 2> /dev/null`" == "" ]; then
-  echo "HLS list is empty! Exiting...."
+if [ "$(cat "$tmpdir"/hls.m3u8 2> /dev/null)" = "" ]; then
+  echo "Format list is empty! Exiting...."
   exit 1
 fi
 
-cat $tmpdir/hls.m3u8 | grep hls- | cut -d '-' -f 2 | cut -d '.' -f 1 > $tmpdir/hlsq_list.txt
-if [ "`cat $tmpdir/hlsq_list.txt 2> /dev/null`" == "" ]; then
-  echo "HLS list (text) is empty! Exiting...."
+< "$tmpdir"/hls.m3u8 grep hls- | cut -d '-' -f 2 | cut -d '.' -f 1 > "$tmpdir/"hlsq_list.txt
+if [ "$(cat "$tmpdir"/hlsq_list.txt 2> /dev/null)" = "" ]; then
+  echo "Format list (text) is empty! Exiting...."
   exit 1
 fi
 
-if [ "$quality" == "high" ]; then
-  hlsq_raw="`cat $tmpdir/hlsq_list.txt | sort -g | tail -1`"
-elif [ "$quality" == "low" ]; then
-  hlsq_raw="`cat $tmpdir/hlsq_list.txt | sort -g | head -1`"
+if [ "$quality" = "high" ]; then
+  hlsq_raw="$(< "$tmpdir"/hlsq_list.txt sort -g | tail -1)"
+elif [ "$quality" = "low" ]; then
+  hlsq_raw="$(< "$tmpdir"/hlsq_list.txt sort -g | head -1)"
 fi
 
-if [ "$hlsq_raw" == "" ]; then
-  PS3='Select the download quality: '
-  readarray -t options < $tmpdir/hlsq_list.txt
-  select opt in "${options[@]}"; do
-  IFS=' ' read name choice <<< $opt
-    case $opt in
-      $opt)
-        hlsq_raw="$opt"
-        break
-        ;;
-    esac
+if [ "$hlsq_raw" = "" ]; then
+  nl -s ". " -w 1 "$tmpdir"/hlsq_list.txt
+  count="$(wc -l "$tmpdir"/hlsq_list.txt | cut -f 1 -d' ')"
+  while true; do
+    printf "Select option: "
+    read -r opt
+    if [ "$opt" -eq "$opt" ] 2>/dev/null && [ "$opt" -gt 0 ] 2>/dev/null && [ "$opt" -le "$count" ] 2>/dev/null; then
+      break
+    fi
   done
-  if [ "$opt" == "" ]; then
-    echo "Invalid option selected. Try again!"
-    exit 1
-  fi
+  value="$(sed -n "${opt}p" "$tmpdir"/hlsq_list.txt)"
+  hlsq_raw="$value"
+  echo "Proceeding with $value...."
 fi
 
 hlsq="hls-$hlsq_raw.m3u8?$tokens"
 hlsqf="hls-$hlsq_raw.m3u8"
-hlsq_path="`echo $hls_url | cut -d '?' -f 1`"
-hlsq_path="`echo ${hlsq_path::-8}`"
+hlsq_path="$(echo "$hls_url" | cut -d '?' -f 1)"
+hlsq_path="${hlsq_path%????????}"
 hlsq_url="$hlsq_path$hlsq"
 
-echo -n "Requesting HLS playlist.... "
-curl -o $tmpdir/$hlsqf --silent $hlsq_url
-if [ "`cat $tmpdir/$hlsqf 2> /dev/null`" == "" ]; then
+printf "Requesting HLS playlist.... "
+curl -o "$tmpdir"/"$hlsqf" --silent "$hlsq_url"
+if [ "$(cat "$tmpdir"/"$hlsqf" 2> /dev/null)" = "" ]; then
   echo "Failed! Exiting...."
   exit 1
 fi
 echo "Done!"
 
-parts="`cat $tmpdir/$hlsqf | grep hls-`"
-num_parts="`echo $parts | wc -w`"
+parts="$(<"$tmpdir"/"$hlsqf" grep hls-)"
+num_parts="$(echo "$parts" | wc -w)"
 
 i=0
 for part in $parts; do
-  i="`expr $i + 1`"
-  partn="`echo $part | cut -d '?' -f 1`"
-  response="`curl -I --silent $hlsq_path$part`"
-  remote_file_size="`echo \"$response\" | grep 'Content-Length' | awk '{print $2}'`"
-  native_file_size="`stat --printf=\"%s\" $tmpdir/$partn 2> /dev/null`"
-  if [ "$native_file_size" == "" ]; then
+  i="$((i + 1))"
+  partn="$(echo "$part" | cut -d '?' -f 1)"
+  response="$(curl -I --silent "$hlsq_path""$part")"
+  remote_file_size="$(echo "$response" | grep 'Content-Length' | awk '{print $2}')"
+  native_file_size="$(stat --printf="%s" "$tmpdir"/"$partn" 2> /dev/null)"
+  if [ "$native_file_size" = "" ]; then
     native_file_size="0"
   fi
   if [ ! -f "$tmpdir/$partn" ]; then
     download_part
   else
-    if [ "$native_file_size" == "$remote_file_size" ]; then
+    if [ "$native_file_size" -eq "$remote_file_size" ]; then
       echo "$partn Already downloaded. Continuing.... ($i/$num_parts)"
     else
       download_part
@@ -156,19 +154,19 @@ done
 
 concat_parts=""
 for part in $parts; do
-  partn="`echo $part | cut -d '?' -f 1`"
-  if [ "$concat_parts" == "" ]; then
+  partn="$(echo "$part" | cut -d '?' -f 1)"
+  if [ "$concat_parts" = "" ]; then
     concat_parts="$tmpdir/$partn"
   else
     concat_parts="$concat_parts|$tmpdir/$partn"
   fi
 done
 
-echo -n "Combining parts.... "
+printf "Combining parts.... "
 filename="${video_id}_${hlsq_raw}.mp4"
 i=0
 while [ -f "$filename" ]; do
-  i="`expr $i + 1`"
+  i="$((i + 1))"
   filename="${video_id}_${hlsq_raw}(${i}).mp4"
 done
 ffmpeg -hide_banner -loglevel panic -i "concat:$concat_parts" -c copy "$filename"
@@ -187,7 +185,8 @@ if [ ! -f "$filename" ]; then
 fi
 echo "Done!"
 
-echo -n "Cleaning up temporary files.... "
+printf "Cleaning up temporary files.... "
 sleep 1
-rm -rf $tmpdir
+rm -rf "$tmpdir"
+rm "$filename" # DEBUG ------------------------------------------------------------------------------
 echo "Done!"
